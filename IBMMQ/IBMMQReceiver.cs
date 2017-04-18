2ba916c02ab9d524f2dd0a66f152eb42b57ffd89
tender4ever <tender4ever.young@gmail.com>
2017-04-18 17:12:03 +0800

    Add files via upload

---
+++ IBMMQ/IBMMQReceiver.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IBM.WMQ;
+using AUOMQ.AUOInterface;
+using System.Threading;
+
+namespace AUOMQ.IBMMQ
+{
+    public class IBMMQReceiver : MQreceiver
+    {
+        private String hostName = "192.168.222.135";    //hostname
+        private String channelName = "mychannel";       //channel Name
+        private int port = 1414;                        //port
+        private String queueManagerName = "mq_app";     //queueManager Name
+        private String queueName = "myqueue";           //queue Name
+
+        private String connectionStatus = "NotReady";   //連線狀態
+
+        private MQQueueManager queueManager;            //佇列管理程式
+        private MQQueue receivequeue;                      //建立queue
+        private MQGetMessageOptions queueGetMessage;    //put Message
+        private MQMessage message;                      //建立message
+
+        private MQEventListener listener;
+
+        //建構子
+        public IBMMQReceiver(MQEventListener eventlistener,String hostName,String channelName,int port ,String queueName)
+        {
+            this.hostName = hostName;
+            this.channelName = channelName;
+            this.port = port;
+            this.queueName = queueName;
+            MQEnvironment.Hostname = hostName;
+            MQEnvironment.Channel = channelName;
+            MQEnvironment.Port = port;
+
+            listener = eventlistener;
+
+            connect();//連線
+            Thread startreceive = new Thread(receive);
+            startreceive.Start();
+        }
+        //建構子
+        public IBMMQReceiver(MQEventListener eventlistener)
+        {
+            MQEnvironment.Hostname = hostName;
+            MQEnvironment.Channel = channelName;
+            MQEnvironment.Port = port;
+
+            listener = eventlistener;
+
+            connect();//連線
+            Thread startreceive = new Thread(receive);
+            startreceive.Start();
+        }
+        //連線
+        public void connect()
+        {
+            try
+            {
+                queueManager = new MQQueueManager(queueManagerName);
+                receivequeue = queueManager.AccessQueue(queueName, MQC.MQOO_INPUT_AS_Q_DEF + MQC.MQOO_FAIL_IF_QUIESCING);
+                connectionStatus = "Ready";
+
+                listener.onConnect("receiver connect ok");
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+                System.Console.WriteLine("connect fail");
+                disconnect();
+            }
+
+        }
+        //斷線
+        public void disconnect()
+        {
+            try
+            {
+                receivequeue.Close();
+                receivequeue = null;
+                connectionStatus = "NotReady";
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+                System.Console.WriteLine("disconnect fail");
+            }
+        }
+
+        public void receive() {
+            while (true) {
+                try
+                {
+                    if (connectionStatus == "Ready")
+                    {
+                        queueGetMessage = new MQGetMessageOptions();
+
+                        message = new MQMessage();
+                        message.Format = MQC.MQFMT_STRING;
+                        receivequeue.Get(message, queueGetMessage);
+
+                        String txt = message.ReadString(message.DataLength);
+
+                        if (listener != null)
+                        {
+                            try
+                            {
+                                listener.onMessage(txt);
+                            }
+                            catch (Exception e)
+                            {
+                                e.Message.ToString();
+                            }
+                        }
+                    }
+                    else {
+                        System.Console.WriteLine("Connection Status not Ready");
+                        disconnect();
+                    }
+                }
+                catch (Exception e) {
+                    e.ToString();
+                }
+            }
+        }
+
+        public String getConnectionStatus()
+        {
+            return connectionStatus;
+        }
+
+        public void setConnectionStatus(String connectionStatus)
+        {
+            this.connectionStatus = connectionStatus;
+        }
+    }
+}
---
+++ IBMMQ/IBMMQSender.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IBM.WMQ;
+using AUOMQ.AUOInterface;
+
+namespace AUOMQ.IBMMQ 
+{
+    public class IBMMQSender : MQsender //實作
+    {
+        private String hostName = "192.168.222.135";    //hostname
+        private String channelName = "mychannel";       //channel Name
+        private int port = 1414;                        //port
+        private String queueManagerName = "mq_app";     //queueManager Name
+        private String queueName = "myqueue";           //queue Name
+
+        private String connectionStatus = "NotReady";   //連線狀態
+
+        private MQQueueManager queueManager;            //佇列管理程式
+        private MQQueue sendqueue;                      //建立queue
+        private MQPutMessageOptions queuePutMessage;    //put Message
+        private MQMessage message;                      //建立message
+        
+        //建構子
+        public IBMMQSender(String hostName) {
+            this.hostName = hostName;
+            MQEnvironment.Hostname = hostName;
+            MQEnvironment.Channel = channelName;
+            MQEnvironment.Port = port;
+
+            connect();//連線
+        }
+        //建構子
+        public IBMMQSender()
+        {
+            MQEnvironment.Hostname = hostName;
+            MQEnvironment.Channel = channelName;
+            MQEnvironment.Port = port;
+
+            connect();//連線
+        }
+
+        //連線
+        public void connect() {
+            try
+            {
+                queueManager = new MQQueueManager(queueManagerName);
+                sendqueue = queueManager.AccessQueue(queueName, MQC.MQOO_OUTPUT + MQC.MQOO_FAIL_IF_QUIESCING);
+                connectionStatus = "Ready";
+
+            }
+            catch (Exception e) {
+                e.ToString();
+                System.Console.WriteLine("connec fail");
+                disconnect();
+            }
+
+        }
+
+        //斷線
+        public void disconnect() {
+            try
+            {
+                sendqueue.Close();
+                sendqueue = null;
+                connectionStatus = "NotReady";
+            }
+            catch (Exception e) {
+                e.ToString();
+                System.Console.WriteLine("disconnec fail");
+            }
+        }
+
+        public void send(String InputText) {
+            try
+            {
+                if (connectionStatus == "Ready")
+                {
+                    queuePutMessage = new MQPutMessageOptions();
+
+                    message = new MQMessage();
+                    message.WriteString(InputText);
+                    message.Format = MQC.MQFMT_STRING;
+                    sendqueue.Put(message, queuePutMessage);
+
+                    disconnect();
+                }
+                else {
+                    System.Console.WriteLine("Connection Status not Ready");
+                    disconnect();
+                }
+            }
+            catch (Exception e) {
+                e.ToString();
+            }
+        }
+
+        public String getConnectionStatus()
+        {
+            return connectionStatus;
+        }
+
+        public void setConnectionStatus(String connectionStatus) {
+            this.connectionStatus = connectionStatus;
+        }
+    }
+}
