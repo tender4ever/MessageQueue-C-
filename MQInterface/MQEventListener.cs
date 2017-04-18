ae5586d50f2663f42ce5469e2fd5650c425fee4b
tender4ever <tender4ever.young@gmail.com>
2017-04-18 17:11:28 +0800

    Add files via upload

---
+++ MQInterface/MQEventListener.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Messaging; 
+
+namespace AUOMQ.AUOInterface
+{
+    public interface MQEventListener
+    {
+        void onMessage(String txtMessage);
+        void onConnect(String status);
+
+    }
+}
---
+++ MQInterface/MQreceiver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Messaging;
+
+namespace AUOMQ.AUOInterface
+{
+    public interface MQreceiver
+    {
+        void connect();
+        void disconnect();
+        void receive();
+        String getConnectionStatus();
+        void setConnectionStatus(String connectionStatus);
+    }
+}
